206312d624bd7b941138189a1898d6338d1dc733
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2023-07-09 16:23:41 +0530

    footer ready "static message"

--- pairllm/src/App.jsx
+++ pairllm/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ReactDOM } from "react";
 import Navbar from './Navbar';
+import Footer from "./Footer";
 
 export default function App(){
   return(
     <div>
       {/* <h1>Hello Sid! Let's do this! Let's go!</h1> */}
       <Navbar/>
+      <Footer/>
     </div>
   )
 }
---
+++ pairllm/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+const footer = ()=>{
+    return(
+        <footer>
+            <h3>let's go!</h3>
+        </footer>
+    )
+}
+
+export default footer;
