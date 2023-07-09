3b83db2fbfff845527fe526a6557a4e3262db8b1
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2023-07-09 14:40:51 +0530

    navbar ready

---
+++ pairllm/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactDOM } from "react";
+import Navbar from './Navbar';
+
+export default function App(){
+  return(
+    <div>
+      {/* <h1>Hello Sid! Let's do this! Let's go!</h1> */}
+      <Navbar/>
+    </div>
+  )
+}
---
+++ pairllm/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = () =>{
+    return(
+        <nav>
+            <h1 id="nav-title">PairLLM</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
