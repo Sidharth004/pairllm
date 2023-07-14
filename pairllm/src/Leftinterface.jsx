385b564cfd5f74761e378801e09a6ce454f9f46c
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2023-07-14 22:49:22 +0530

    left side chat functional messaging and styling

--- pairllm/src/Leftinterface.jsx
+++ pairllm/src/Leftinterface.jsx
@@ -1,22 +1,75 @@
-import React from "react";
+import { useState} from "react";
 import './lhs.css';
 
 const Leftinterface = () =>{
+
+    //add state for input and chat log
+    const [input,setInput] = useState("");
+    const [chatLog , setChatLog] = useState([{
+        user: "gpt",
+        message: "Hi Sid! Let's do this!"
+    },{
+        user: "me",
+        message: "Hi gpt!"
+    }]);
+
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setChatLog([...chatLog,{user: "me",message: `${input}`}])
+        console.log("submit")
+        setInput("");
+        
+
+    }
+    
     return(
         <div>
+            
             <div className="chat-box">
 
-                <div className="chat-log1">
-                    <div className="chat-message">
-                        <div className="test">
+                <div className="prompt_area">
+                    <form onSubmit={handleSubmit}>
+                    <input 
+                        value={input}
+                        onChange={
+                            (e)=> setInput(e.target.value)
+                        }
+                        className="prompt_area_field" 
+                        placeholder=" lets do this!" 
+                        rows="1" ></input>
+                    </form>
+                    
+                </div>
 
-        
-                            <div className="avatar">
-                                
+                <div className="chat-log1">
+                    {chatLog.map((message,index)=>  (
+                        <ChatMessage key={index} message={message}/>
+                    ))}
+                    
+                    {/* <div className="chat-message chatgpt">
+                        <div className="test">
+                            <div className="avatar chatgpt">
+                             yo
                             </div>
                             <div className="message">
-                                Hi there! Lets build this thing!
-                                
+                            Hi I am PairLLM!
+                            </div>
+                        </div>
+                    </div>  */}
+                </div>
+
+{/* ........................................................................................ */}
+
+
+                <div className="chat-log2">
+                    <div className="chat-message">
+                        <div className="test">
+                            <div className="avatar">
+
+                            </div>
+                            <div className="message">
+                                Hi there! Lets build this thing! 
                             </div>
                         </div>
                     </div>
@@ -29,23 +82,7 @@ const Leftinterface = () =>{
                             Hi I am PairLLM!
                             </div>
                         </div>
-                        
-                    </div>
-
-
-                    
-                </div>
-
-
-
-
-                <div className="chat-log2">
-                    <div className="avatar">
-                        Me
-                    </div>
-                    <div className="chat-message">
-                        Hi there! Lets build this thing!
-                    </div>
+                    </div> 
                 </div>
 
             </div>
@@ -53,4 +90,20 @@ const Leftinterface = () =>{
     )
 }
 
+
+const ChatMessage =({message }) =>{
+    return(
+        <div className= {` chat-message ${message.user == "gpt" && "chatgpt"}`}>
+                        <div className="test">
+                            <div className= {`avatar ${message.user == "gpt" && "chatgpt"}`}>
+
+                            </div>
+                            <div className="message">
+                                {message.message}
+                                {/* Hi there! Lets build this thing!  */}
+                            </div>
+                        </div>
+                    </div>
+    )
+}
 export default Leftinterface;
