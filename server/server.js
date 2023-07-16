d0b2412b085a8394328163500200e0e8bfacb97c
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2023-07-16 23:40:03 +0530

    frontend connection  established with gpt api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { response } from 'express';
 import * as dotenv from 'dotenv'; 
 import cors from 'cors'; 
+import bodyParser from 'body-parser';
 import {Configuration,OpenAIApi }from 'openai'
 
 dotenv.config();
@@ -17,36 +18,39 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json())
+// app.use(express.json());
+app.use(express.urlencoded({ extended : true}))
 
 //dummy route
 
-app.get('/',async(req,res)=>{
-    res.status(200).send({
-        message:'lets go Sid!',
-    })
-});
+//  
 
 app.post('/',async(req,res)=>{
     try {
-        const propmt = req.body.propmt;
-
-        
+        // const propmt = req.body.propmt;
+         const {message} = req.body;
+        //  console.log(message)
          const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `${prompt}`,
-                //prompt:'Say this is a test',
-                temperature: 0.58,
-                max_tokens: 3500,
-                top_p: 1,
-                frequency_penalty: 0,
-                presence_penalty: 0,
+                 model: "text-davinci-003",
+                 prompt: `${message}`,
+                 
+                 temperature: 0.58,
+                 max_tokens: 3500,
+                 top_p: 1,
+                 frequency_penalty: 0,
+                 presence_penalty: 0,
                 
-            });
-            console.log(response.data.choices[0].text)
-        res.status(200).send({
-            bot: response.data.choices[0].text 
-        })
+             });
+            //  console.log()
+
+        res.json({
+            // data: response.data
+            message:response.data.choices[0].text,
+        }) 
+        // res.status(200).send({
+        //     bot: response.data.choices[0].text 
+        // })
     } catch (error) {
         console.log(error);
         res.status(500).send({ error })
