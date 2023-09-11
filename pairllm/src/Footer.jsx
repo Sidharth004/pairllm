fe1165f87062f7ee3ec03d150d5cc8b487a9d8d3
SIDHARTH KUMTHEKAR <kumthekarsid@gmail.com>
2023-09-11 18:59:23 +0530

    footer fixes

--- pairllm/src/Footer.jsx
+++ pairllm/src/Footer.jsx
@@ -5,9 +5,13 @@ const footer = ()=>{
     return(
         <footer>
             {/* <h3>let's go!</h3> */}
-            <h3>Made with love by Sid!!</h3>
-            <p>Github repo</p>
-            <p>Video tutorial</p>
+            <div className="footer-content">
+                
+                <p>Github repo</p>
+                <h3>Made with love by Sid!!</h3>
+                <p>Video tutorial</p>
+            </div>
+            
             {/* <h3>For video trial click here</h3>
             <h3>Github</h3> */}
         </footer>
